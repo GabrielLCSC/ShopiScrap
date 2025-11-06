f5c7ac2c2ec29afc8eb6479dbc2aef9af9278440
gabriellcsc <gabriel.dautreppe@gmail.com>
2025-11-06 22:04:26 +0100

    feat(next): feat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import SessionProvider from "@/components/SessionProvider";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -66,9 +67,11 @@ export default function RootLayout({
           <Header />
           <main className="flex-1">
             {children}
+            
           </main>
           <Footer />
         </SessionProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
